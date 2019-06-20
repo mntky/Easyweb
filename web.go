a06bfd47a571a017e518735645e4187ce3c85f86
mntky <djminatoo@gmail.com>
2019-06-20 12:14:33 +0900

    [add]-t args, errlog [chg]separate start func

--- web.go
+++ web.go
@@ -8,6 +8,7 @@ import (
 	"log"
 	"io"
 	"strconv"
+	"regexp"
 
 )
 
@@ -15,25 +16,38 @@ import (
 var (
 	addr = flag.String("a", "127.0.0.1", "-a options is Listen address")
 	port = flag.Int("p", 8080, "-p options is Listen Port")
+	args = flag.String("t", "Test Page", "-t options is display words")
 )
 
 //logger
 var (
-	buf bytes.Buffer
-	logger = log.New(&buf, "[error]", log.Lshortfile)
+	errbuf bytes.Buffer
+	errlog = log.New(&errbuf, "[error]", 5)
+	//log.New(io.write, string,	0 -- nothing
+	//							1 -- yyyy/mm/dd
+	//							2 -- hh:mm:ss
+	//							3 -- day and time
+	//							4 -- print the 6th decimal place
+	//							5 -- day and [4]
 )
 
 func main() {
 	flag.Parse()
-	fmt.Printf("v: %v	p: %v\n", *addr, &addr)
-	fmt.Printf("v: %v		p: %v\n", *port, &port)
-	fmt.Printf("Listen Start %v:%v\n", *addr,*port)
-
-	handle := func(w http.ResponseWriter, r *http.Request) {
-		io.WriteString(w, "Test Page\n")
+	chk := regexp.MustCompile(`\d+\.\d+\.\d+\.\d+`)
+	tf := chk.MatchString(*addr)
+	if tf == false {
+		errlog.Print("mistaken ip address")
+		fmt.Println(&errbuf)
 	}
 
-	http.HandleFunc("/", handle)
-	http.ListenAndServe(*addr + ":" + strconv.Itoa(*port) , nil)
+	fmt.Printf("Listen Start %v:%v\n", *addr,*port)
+	start()
 }
 
+func start() {
+	handle := func(w http.ResponseWriter, r *http.Request) {
+		io.WriteString(w, *args)
+	}
+	http.HandleFunc("/", handle)
+	http.ListenAndServe(*addr + ":" + strconv.Itoa(*port) ,nil)
+}
